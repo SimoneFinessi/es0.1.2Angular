2db77f021f3cf90544e8c131c9779d28f6461d2e
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-11-30 13:31:29 +0000

    punto2

--- Finessi/src/app/app.module.ts
+++ Finessi/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimoComponent } from './primo/primo.component';
+import { SecondoComponent } from './secondo/secondo.component';
+import { TerzoComponent } from './terzo/terzo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimoComponent,
+    SecondoComponent,
+    TerzoComponent
   ],
   imports: [
     BrowserModule
